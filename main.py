06aec32bbfd71e4d0435f9e0f82184fadb9fdaa5
constanca-m <constanca.manteigas@elastic.co>
2023-06-29 19:39:20 +0200

    Remove max docs default.

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     client = get_client(elasticsearch_host, elasticsearch_ca_path, elasticsearch_user, elasticsearch_pwd, cloud_id, elastic_pwd)
     print("You're testing with version {}.\n".format(client.info()["version"]["number"]))
 
-    all_placed = copy_from_data_stream(client, data_stream, max_docs=40000)#, max_docs=20000)
+    all_placed = copy_from_data_stream(client, data_stream)
 
     ## Is running this function too slow? Please set the number of max documents as this:
     ## all_placed = copy_from_data_stream(client, data_stream, max_docs=5000)
